8b196db73fd5afd22e2336e7ad8fa35f785bbce5
Alaukik Miishra <151136125+Alaukik1210@users.noreply.github.com>
2026-01-12 20:58:46 +0530

    about animation done

--- app/about/our-purpose/page.tsx
+++ app/about/our-purpose/page.tsx
@@ -7,7 +7,7 @@ import OurValuesScrollSection from "@/components/OurValuesScrollSection";
 
 export default function OurPurposePage() {
   return (
-    <div className="bg-white">
+    <div className="bg-white ">
       <Navbar appearance="page-gradient-static" />
 
       {/* Hero Section */}
@@ -249,32 +249,33 @@ export default function OurPurposePage() {
               className="absolute inset-0 h-full w-full object-cover will-change-transform z-30"
               muted
               loop
+              autoPlay
               playsInline
               preload="auto"
               controls={false}
               aria-label="Background hero video"
             >
-              <source src="/videos/hero-desktop.mp4" type="video/mp4" />
+              <source src="https://download-video-ak.vimeocdn.com/v3-1/playback/572a841d-eea5-4838-95da-6734b47cc0ae/12c0fad1-cbae0fcb?__token__=st=1768230667~exp=1768234267~acl=%2Fv3-1%2Fplayback%2F572a841d-eea5-4838-95da-6734b47cc0ae%2F12c0fad1-cbae0fcb%2A~hmac=0e120a429ba41a4553670dad0ab86189c6a0339c0109827c4abc33965c4845e3&r=dXMtZWFzdDE%3D" type="video/mp4" />
               Your browser does not support the video tag.
             </video>
           </div>
         </section>
-        <div className=" max-w-7xl">
-          <h4 className="text-[#023B66] font-alternate font-medium md:pl-32 tracking-widest uppercase  md:pt-12 text-5xl md:text-7xl lg:text-[145px] leading-none">
+        <div className=" max-w-7xl  mx-auto">
+          <h4 className="text-[#023B66] font-alternate font-medium tracking-widest uppercase  md:pt-12 text-5xl md:text-7xl lg:text-[145px] leading-none">
             OUR JOURNEY
           </h4>
         </div>
 
         {/* Why The Name Section */}
-        <div className="relative bg-white py-12 md:py-16 lg:py-24 px-4 md:px-8 lg:px-32">
+        <div className="relative bg-white py-12 md:py-16 lg:py-24 max-w-7xl mx-auto ">
           <div className="container mx-auto px-4">
-            <div className="grid grid-cols-1 lg:grid-cols-12 gap-8 lg:gap-16 items-start">
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-16 items-start">
               {/* Left Content */}
-              <div className="space-y-4 md:space-y-6 order-2 lg:order-1 lg:col-span-4 lg:pl-28">
+              <div className="">
                 <h2 className="text-2xl md:text-4xl lg:text-5xl font-medium leading-tight md:leading-widest uppercase">
                   Why the name?
                 </h2>
-                <p className="text-xs md:text-sm tracking-wider font-medium uppercase text-gray-600">
+                <p className="text-xs md:text-sm tracking-wider font-medium uppercase text-gray-500">
                   RARE & REMARKABLE
                 </p>
                 <p className="leading-relaxed text-gray-400 text-sm md:text-base lg:text-md">
@@ -290,14 +291,14 @@ export default function OurPurposePage() {
               </div>
 
               {/* Right Image */}
-              <div className="relative order-1 lg:order-2 -mt-10 md:-mt-48 lg:-mt-40 z-20 w-full lg:w-[30vw] lg:absolute lg:right-0">
+              <div className="relative order-1 lg:order-2 -mt-10 z-0 md:-mt-48 lg:-mt-40 w-full lg:w-[35vw] lg:absolute lg:-right-24">
                 <Image
                   src="/images/about/bg14.jpg"
                   alt="Canyon with Black Tomato branding"
-                  height={400}
-                  width={800}
-                  className="object-contain w-full md:w-[80%] lg:w-[80%] shadow-2xl"
-                  sizes="(max-width: 768px) 100vw, (max-width: 1200px) 60vw, 800px"
+                  height={550}
+                  width={1150}
+                  className="object-contain w-full md:w-[85%] lg:w-[85%] shadow-2xl"
+                  sizes="(max-width: 768px) 100vw, (max-width: 1200px) 70vw, 900px"
                 />
               </div>
             </div>
@@ -305,38 +306,56 @@ export default function OurPurposePage() {
         </div>
 
         {/* Your World Your Trips Section */}
-        <div className="relative pb-20 md:pb-2 md:mt-20">
-          <div className="container mx-auto px-4 md:px-8 lg:px-32">
-            <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-16 items-center">
-              {/* Left Image */}
-              <div className="relative -mt-10 md:-mt-32 lg:-mt-20 z-10 md:pl-20 w-full aspect-[4/3]">
-                <Image
-                  src="/images/about/bg07.jpg"
-                  alt="Boat on turquoise water"
-                  height={650}
-                  width={450}
-                  className="object-contain"
-                  sizes="(max-width: 768px) 100vw, (max-width: 1024px) 50vw, 500px"
-                />
-              </div>
+        <div className="relative pb-20 md:pb-2 md:mt-60">
+  <div className="container mx-auto px-4 md:px-8 lg:px-32">
+    <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-16 items-center">
+      {/* Left Image */}
+      <div className="relative -mt-10 md:-mt-32 lg:mt-6 z-12 md:pl-20 w-full aspect-[4/3] scale-180 md:ml-20">
+        <Image
+          src="/images/about/bg11.jpg"
+          alt="Boat on turquoise water"
+          height={650}
+          width={450}
+          className="object-contain"
+          sizes="(max-width: 768px) 100vw, (max-width: 1024px) 50vw, 500px"
+        />
+      </div>
 
-              {/* Right Content */}
-              <div className="space-y-4 md:space-y-3 lg:pl-8 order-2 lg:order-1 lg:mb-12">
-                <h2 className="text-2xl md:text-4xl lg:text-5xl font-medium leading-tight md:leading-widest uppercase">
-                  Your world. Your trips.
-                </h2>
-                <p className="text-gray-400 text-sm md:text-base lg:text-md w-full md:w-9/10 leading-relaxed">
-                  Like a perfectly tailored suit, each trip we plan is different;
-                  just as each traveller is different. We&apos;re here to help you
-                  experience the world on your terms – to follow your dreams, to
-                  pursue your passions. With obsessive attention to detail and
-                  extraordinary customer client service, we&apos;re all about the
-                  personal. It&apos;s your world. It&apos;s your trip.
-                </p>
-              </div>
-            </div>
-          </div>
+      {/* Right Content */}
+      <div className="space-y-4 md:space-y-3 lg:pl-52 order-2 lg:order-1 lg:mb-12">
+        <h2 className="text-2xl md:text-4xl lg:text-5xl font-medium leading-tight md:leading-widest uppercase">
+          Your world. Your trips.
+        </h2>
+        <p className="text-gray-400 text-sm md:text-base lg:text-md w-full md:w-9/10 leading-relaxed">
+          Like a perfectly tailored suit, each trip we plan is different;
+          just as each traveller is different. We&apos;re here to help you
+          experience the world on your terms – to follow your dreams, to
+          pursue your passions. With obsessive attention to detail and
+          extraordinary customer client service, we&apos;re all about the
+          personal. It&apos;s your world. It&apos;s your trip.
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
+      </section>
+      <div className="my-4">
+        <div className="uppercase text-3xl  tracking-wider font-brandon font-semibold text-center">
+          feel something new
         </div>
+        <button className="text-white bg-black text-xs px-4 font-medium tracking-wider py-2 mt-4 mx-auto block">
+        OUR TRAVEL CALENDER
+        </button>
+      </div>
+      <section>
+        <Image
+          src="/images/about/bg12.jpg"
+          alt="Scenic landscape"
+          height={600}
+          width={2400}
+          className="object-cover w-full h-auto"
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 100vw, 2400px"
+        />
       </section>
     </div>
   );
