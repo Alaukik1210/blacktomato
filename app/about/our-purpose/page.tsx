3954646f39444a8688b32d0e3835f35e02bce558
Alaukik Miishra <151136125+Alaukik1210@users.noreply.github.com>
2026-01-16 23:16:05 +0530

    making about responsive and fixing it

--- app/about/our-purpose/page.tsx
+++ app/about/our-purpose/page.tsx
@@ -13,11 +13,11 @@ export default function OurPurposePage() {
       {/* Hero Section */}
       <div
         style={{ backgroundImage: "url('/images/about/bg02.jpg')" }}
-        className="min-h-[50vh] sm:min-h-[60vh] lg:min-h-[70vh] bg-cover bg-center bg-no-repeat px-4 sm:px-6 md:px-8 lg:pl-60 relative"
+        className="min-h-[70vh] lg:min-h-[70vh] bg-cover bg-center bg-no-repeat px-4 sm:px-6 md:px-8 lg:pl-60 relative"
       >
         <div className="container md:px-4">
           {/* Heading */}
-          <h4 className="text-[#379C8A] font-alternate font-medium tracking-widest uppercase pt-20 sm:pt-24 md:pt-28 lg:pt-60 text-5xl sm:text-6xl md:text-8xl lg:text-[145px] leading-none">
+          <h4 className="text-[#379C8A] font-alternate font-medium tracking-widest uppercase pt-28 md:pt-28 lg:pt-60 text-8xl md:text-[145px] leading-none">
             ABOUT US
           </h4>
 
@@ -27,14 +27,14 @@ export default function OurPurposePage() {
             <div className="text-white hidden lg:block space-y-6 lg:pr-8 text-right"></div>
 
             {/* Main Content */}
-            <div className="text-white space-y-3 sm:space-y-4 md:space-y-5 lg:space-y-6 lg:mt-2 md:mt-12 lg:pr-8 text-left md:text-left max-w-full md:max-w-2xl lg:max-w-none">
+            <div className="text-white space-y-3 sm:space-y-4 md:space-y-5 lg:space-y-6 mt-36 lg:mt-2 md:mt-12 lg:pr-8 text-left md:text-left max-w-full md:max-w-2xl lg:max-w-none">
               {/* Small heading */}
-              <p className="text-xs tracking-wider font-semibold uppercase text-[#379C8A]">
+              <p className="text-sm tracking-wider font-semibold uppercase text-[#379C8A]">
                 ABOUT US
               </p>
 
               {/* Large heading */}
-              <h4 className="text-3xl sm:text-4xl tracking-wider md:text-5xl lg:text-5xl font-medium uppercase leading-tight">
+              <h4 className="text-4xl sm:text-4xl tracking-wider md:text-5xl lg:text-5xl font-medium uppercase leading-tight">
                 AWARD-WINNING CREATORS OF
                 <br className="hidden sm:block" />
                 <span className="sm:hidden"> </span>
@@ -49,10 +49,10 @@ export default function OurPurposePage() {
 
       {/* First Content Section - Overlapping Layout */}
       <div className="pb-12 sm:pb-16 md:pb-20 lg:pb-2">
-        <div className="container lg:mx-auto px-4 sm:px-6 md:px-8 lg:px-32">
+        <div className="container lg:mx-auto px-4 sm:px-6 md:px-8 lg:px-24">
           <div className="grid grid-cols-1 md:grid-cols-[400px_1fr] lg:grid-cols-[500px_1fr] md:gap-4 lg:gap-16 items-center">
             {/* Left Image */}
-            <div className="relative -mt-8 sm:-mt-10 md:-mt-20 lg:-mt-60 z-10 w-full md:w-[400px] lg:w-[500px] aspect-[4/3]">
+            <div className="relative -mt-8 sm:-mt-10 md:-mt-40 lg:-mt-60 z-10  pr-60 w-[670px] md:w-[620px] md:h-[700px] lg:w-[800px] aspect-[4/3]">
               <Image
                 src="/images/about/Image.jpg"
                 alt="Dramatic mountain landscape"
@@ -63,18 +63,18 @@ export default function OurPurposePage() {
             </div>
 
             {/* Right Content */}
-            <div className="space-y-3 sm:space-y-4 md:space-y-3 lg:pl-8 order-2 lg:order-1 lg:mb-12 mt-6 sm:mt-8 md:mt-0">
-              <p className="text-xs sm:text-sm md:text-sm tracking-wider font-semibold uppercase text-gray-800">
+            <div className="space-y-3 sm:space-y-4 md:space-y-3 px-6 md:px-0 lg:pl-20 order-2 lg:order-1 lg:mb-12 mt-6 sm:mt-8 md:-mt-24 lg:mt-0">
+              <p className="text-md sm:text-md md:text-sm tracking-wider font-semibold uppercase text-gray-800">
                 WHAT WE&apos;RE ABOUT
               </p>
-              <h2 className="text-3xl sm:text-4xl md:text-5xl lg:text-5xl font-medium tracking-wider leading-tight md:leading-widest uppercase">
+              <h2 className="text-4xl sm:text-4xl md:text-5xl lg:text-5xl font-medium lg:tracking-wider tracking-wider lg:leading-widest uppercase">
                 WE&apos;RE A TEAM&nbsp;
                 <br className="hidden lg:block" />
                 OF PEOPLE IN THE&nbsp;
                 <br className="hidden lg:block" />
                 KNOW ABOUT TRAVEL
               </h2>
-              <p className="text-gray-500 text-sm sm:text-base md:text-base lg:text-md w-full sm:w-[95%] md:w-full lg:w-4/5 leading-relaxed lg:leading-loose">
+              <p className="text-gray-500 text-md sm:text-base md:text-base  lg:text-md w-full sm:w-[95%] md:w-full lg:w-4/5 leading-relaxed lg:leading-loose">
                 And we&apos;re on a mission to create remarkable experiences
                 that thoughtfully connect our clients to the world and inspire
                 them to explore it with curiosity, humility and a sense of
@@ -86,22 +86,22 @@ export default function OurPurposePage() {
       </div>
 
       {/* Second Content Section - What We're About */}
-      <div className="relative bg-white py-12 sm:py-14 md:py-16 lg:py-24 px-4 sm:px-6 md:px-8 lg:px-32">
+      <div className="relative bg-white py-12 sm:py-14 md:py-16 mt-4 md:mt-20 lg:mt-0  lg:py-24 px-4 sm:px-6 md:px-8 lg:px-32 ">
         <div className="container md:mx-auto px-4 max-w-full">
           <div className="grid grid-cols-1 lg:grid-cols-12 gap-8 lg:gap-16 items-start">
             {/* Left Content */}
-            <div className="space-y-4 sm:space-y-5 md:space-y-6 order-2 lg:order-1 lg:col-span-4 lg:pl-28">
-              <h2 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-medium leading-tight md:leading-widest uppercase">
+            <div className="space-y-4 sm:space-y-5 md:space-y-6 order-2 w-full md:w-4/5 lg:order-1 lg:col-span-4 lg:pl-28">
+              <h2 className="text-4xl  md:text-5xl lg:text-5xl font-medium leading-tight md:leading-widest uppercase">
                 It began with a feeling
               </h2>
-              <p className="text-xs sm:text-xs md:text-sm tracking-wider font-medium uppercase text-gray-600">
+              <p className="text-xs sm:text-xs md:text-sm tracking-wider font-medium uppercase text-gray-500">
                 Black Tomato was founded in 2005 by Tom, James and Matt
               </p>
-              <p className="leading-relaxed text-gray-400 text-sm sm:text-sm md:text-base lg:text-md">
+              <p className="leading-relaxed text-gray-600 text-sm sm:text-sm md:text-base lg:text-md">
                 A trio of friends who wanted to sweep aside the prevailing
                 current of &apos;package&apos; trips and same-same vacations.
               </p>
-              <p className="leading-relaxed text-gray-400 text-sm sm:text-sm md:text-base lg:text-md">
+              <p className="leading-relaxed text-gray-600 text-sm sm:text-sm md:text-base lg:text-md">
                 Since then, we&apos;ve become award-winning curators of
                 tailor-made travel experiences – all crafted with inspirational
                 engineering and an incomparable attention to detail. For us, the
@@ -111,7 +111,7 @@ export default function OurPurposePage() {
             </div>
 
             {/* Right Image */}
-            <div className="relative order-1 lg:order-2 -mt-8 sm:-mt-10 md:-mt-48 lg:-mt-40 z-20 w-full lg:w-[60vw] lg:absolute lg:right-0">
+            <div className="relative order-1 lg:order-2 -mt-8 sm:-mt-10 lg:ml-0 md:ml-68 md:-mt-88 lg:-mt-40 z-20 w-full md:w-[620px] lg:w-[60vw] lg:absolute lg:right-0">
               <Image
                 src="/images/about/bg03.jpg"
                 alt="Coastal mountain view"
@@ -127,23 +127,23 @@ export default function OurPurposePage() {
 
       {/* Third Section */}
       <div className="relative pb-12 sm:pb-16 md:pb-20 lg:pb-2 md:mt-20">
-        <div className="container mx-auto px-4 sm:px-6 md:px-8 lg:px-32">
-          <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 sm:gap-8 lg:gap-16 items-center">
+        <div className="container lg:mx-auto px-4 sm:px-6 md:px-8 lg:px-32">
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-6  sm:gap-8 lg:gap-16 lg:items-center">
             {/* Left Image */}
-            <div className="relative -mt-8 sm:-mt-10 md:-mt-32 lg:-mt-20 z-10 w-full md:pl-0 lg:pl-20 aspect-[4/3]">
+            <div className="relative -mt-8 sm:-mt-10 md:-mt-32 lg:-mt-20 z-10 w-full lg:scale-120 md:pl-0 lg:pl-20 aspect-[4/3]">
               <Image
                 src="/images/about/Image-3.jpg"
                 alt="Dramatic mountain landscape"
                 height={650}
                 width={450}
-                className="object-contain w-full h-full"
+                className="object-contain"
                 sizes="(max-width: 768px) 100vw, (max-width: 1024px) 50vw, 500px"
               />
             </div>
 
             {/* Right Content */}
-            <div className="space-y-3 sm:space-y-4 md:space-y-3 lg:pl-8 order-2 lg:order-1 lg:mb-12">
-              <h2 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-medium leading-tight md:leading-widest uppercase">
+            <div className="space-y-3 sm:space-y-4 md:space-y-3 lg:pl-8 order-2 lg:order-1 lg:mb-12 ">
+              <h2 className="text-4xl sm:text-3xl md:text-5xl lg:text-5xl font-medium leading-tight md:leading-widest uppercase">
                 Unique, different, and daring
               </h2>
               <p className="text-xs sm:text-xs md:text-sm tracking-wider font-medium uppercase text-gray-600">
@@ -172,35 +172,35 @@ export default function OurPurposePage() {
 
       {/* Fourth Section */}
       <div className="relative pb-12 sm:pb-16 md:pb-20 lg:pb-2">
-        <div className="container mx-auto px-4 sm:px-6 md:px-8 lg:px-32">
-          <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 sm:gap-8 lg:gap-16 overflow-hidden items-center">
+        <div className="container lg:mx-auto px-4 sm:px-6 md:px-0 lg:px-32 md:max-w-none">
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 sm:gap-8 lg:gap-16 items-center  md:overflow-hidden">
             {/* Left Image */}
-            <div className="relative -mt-8 sm:-mt-10 md:-mt-32 lg:-mt-10 z-8 w-full lg:w-[120%]  aspect-[4/3]">
+            <div className="relative  -mt-8 sm:-mt-10 md:-mt-32 lg:-mt-10 z-8 w-full md:w-screen md:left-1/2 md:right-1/2 md:-ml-[50vw] md:-mr-[50vw] lg:ml-0 lg:mr-0 lg:left-0 lg:right-0 lg:w-[120%] aspect-[4/3]">
               <Image
                 src="/images/about/bg04.jpg"
                 alt="Dramatic mountain landscape"
                 height={1200}
                 width={1200}
-                className="object-contain w-full h-full"
-                sizes="(max-width: 768px) 100vw, (max-width: 1024px) 50vw, 500px"
+                className="object-cover md:object-cover lg:object-contain w-full h-full"
+                sizes="(max-width: 768px) 100vw, (max-width: 1024px) 100vw, 500px"
               />
             </div>
 
             {/* Right Content */}
-            <div className="space-y-3 sm:space-y-4 md:space-y-3 lg:pl-40 order-2 lg:order-1 lg:mb-12">
-              <h2 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-medium leading-tight md:leading-widest uppercase">
+            <div className="space-y-3 sm:space-y-4 md:space-y-3 md:px-8 lg:px-0 lg:pl-40 order-2 lg:order-1 lg:mb-12">
+              <h2 className="text-4xl sm:text-3xl md:text-5xl lg:text-5xl font-medium leading-tight md:leading-widest uppercase">
                 We&apos;ll show you the world
               </h2>
               <p className="text-xs sm:text-xs md:text-sm tracking-wider font-medium uppercase text-gray-600">
                 And in an entirely new light
               </p>
-              <p className="text-gray-400 text-sm sm:text-sm md:text-base lg:text-md w-full leading-relaxed">
+              <p className="text-gray-500 text-sm sm:text-sm md:text-base lg:text-md w-full leading-relaxed">
                 We curate trips for travellers who want to see the world up
                 close – foregoing &apos;tick lists&apos; and tourist traps in
                 favour of deeper and more intimate connections with the world
                 and its cultures.
               </p>
-              <p className="text-gray-400 text-sm sm:text-sm md:text-base lg:text-md w-full leading-relaxed">
+              <p className="text-gray-500 text-sm sm:text-sm md:text-base lg:text-md w-full leading-relaxed">
                 Today, we take people to all seven continents, designing all of
                 our trips from the ground up. This way, no two journeys are ever
                 quite alike. Trekking with the tribes of Borneo; learning the
@@ -215,7 +215,7 @@ export default function OurPurposePage() {
 
       {/* Regenerative Travel Section */}
       <div className="relative bg-white py-12 sm:py-14 md:py-16 lg:py-24 px-4 sm:px-6 md:pl-20 overflow-hidden">
-        <div className="container mx-auto px-4 max-w-full">
+        <div className="container lg:mx-auto px-4 max-w-full">
           <div className="grid grid-cols-1 lg:grid-cols-12 gap-1 items-start">
             {/* Left Content */}
             <div className="space-y-4 sm:space-y-5 md:space-y-6 order-2 lg:order-1 lg:col-span-5 lg:pl-60">
@@ -225,7 +225,7 @@ export default function OurPurposePage() {
               <p className="text-xs sm:text-xs md:text-sm tracking-wider font-medium uppercase text-gray-600">
                 A Lasting Impact For A Changing World
               </p>
-              <p className="leading-relaxed text-gray-400 text-sm sm:text-sm md:text-base lg:text-md">
+              <p className="leading-relaxed text-gray-600 text-sm sm:text-sm md:text-base lg:text-md">
                 At Black Tomato, we know you&apos;re seeking travel experiences that
                 will leave a lasting, positive impact on our planet and the
                 communities that call it home. This is a belief we also share,
